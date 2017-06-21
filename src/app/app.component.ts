c371b288cf57151a328810456fb4cd1adf08513b
RITTER <br440r@att.com>
2017-06-21 11:17:37 -0500

    Factor out search and results into components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,40 +10,11 @@ import 'rxjs/add/operator/map';
 
 export class AppComponent implements OnInit {
 
-  number_data
-  base_url;
-  constructor(http: Http) {
-    this.base_url = 'https://en.wikipedia.org/wiki/';
-    http.get('https://en.wikipedia.org/api/rest_v1/page/related/number')
-      .map(res => res.json())
-      .subscribe(wiki_data => {
-        const data = wiki_data.pages.slice(1,-1);
-        const number_info = Object.entries(data).map(([key, value])=>{
-          let { 
-            'title': link,
-            'thumbnail': {
-              'source': source
-            }={'source': null},
-            'extract': description,
-            'normalizedtitle': title 
-          } = value;
-          console.log(source);
-          return {
-            title,
-            link,
-            description,
-            source
-          }
-        })
-        this.number_data = number_info;
-      })
+  constructor(){
+    
   }
 
   ngOnInit() {
-  }
-
-  link(item) {
-    return this.base_url + item.link;
   }
 
 }
