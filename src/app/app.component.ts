f019625fe2a143819a2d0cab4eb76388ce6668f5
RITTER <br440r@att.com>
2017-06-23 06:43:18 -0500

    Debug style issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,49 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
+import '../styles.scss';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss','../styles.scss']
+  styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent implements OnInit {
 
-  number_data
-  base_url;
-  constructor(http: Http) {
-    this.base_url = 'https://en.wikipedia.org/wiki/';
-    http.get('https://en.wikipedia.org/api/rest_v1/page/related/number')
-      .map(res => res.json())
-      .subscribe(wiki_data => {
-        const data = wiki_data.pages.slice(1,-1);
-        const number_info = Object.entries(data).map(([key, value])=>{
-          let { 
-            'title': link,
-            'thumbnail': {
-              'source': source
-            }={'source': null},
-            'extract': description,
-            'normalizedtitle': title 
-          } = value;
-          console.log(source);
-          return {
-            title,
-            link,
-            description,
-            source
-          }
-        })
-        this.number_data = number_info;
-      })
+  constructor() {
+    
   }
 
   ngOnInit() {
-  }
-
-  link(item) {
-    return this.base_url + item.link;
   }
 
 }
